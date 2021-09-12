24759fbe8032edea5fb9e9917d8755d980dedabd
GreenSulley <sukeycz@gmail.com>
2021-09-12 18:14:58 +0800

    Github Action 自动化构建

--- transfer.py
+++ transfer.py
@@ -1,4 +1,5 @@
 import os
+import time
 import pydocx
 
 for root, dirs, files in os.walk("./1错题"):  # 获取当前文件夹的信息
@@ -6,5 +7,6 @@ for root, dirs, files in os.walk("./1错题"):  # 获取当前文件夹的信息
         if os.path.splitext(file)[1] == ".docx" and "~" not in str(
             file) and "$" not in str(file):
             md = pydocx.PyDocX.to_html(f"{root}/{file}")
-            with open(f"./1错题/track/{os.path.splitext(file)[0]}.html","w",encoding="utf-8") as f:
+            path = f"./1错题/track/{os.path.splitext(file)[0]}.html"
+            with open(path,"w",encoding="utf-8") as f:
                 f.write(md)
