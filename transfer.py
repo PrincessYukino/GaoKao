462edbc9e6ccc37fed4e9178fa86397ff519bc4c
槐序二三煊 <sukeycz@gmail.com>
2021-09-12 17:36:04 +0800

    feat: track .docx change

---
+++ transfer.py
@@ -0,0 +1,10 @@
+import os
+import pydocx
+
+for root, dirs, files in os.walk("./1错题"):  # 获取当前文件夹的信息
+    for file in files:  # 扫描所有文件
+        if os.path.splitext(file)[1] == ".docx" and "~" not in str(
+            file) and "$" not in str(file):
+            md = pydocx.PyDocX.to_html(f"{root}/{file}")
+            with open(f"./1错题/track/{os.path.splitext(file)[0]}.html","w",encoding="utf-8") as f:
+                f.write(md)
